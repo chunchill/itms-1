1634592df62603650ecc16349ac7431bb1be9d9f
sean <xujihui1985@outlook.com>
2014-03-01 15:26:34 +0800

    replace href with data-target for bootstrap tab plugins cause it has confilit with angular's route

--- app/app.js
+++ app/app.js
@@ -1,9 +1,11 @@
 'use strict'
 
 angular.module('itmsApp',['ngRoute'])
-    .config(function($routeProvider){
+    .config(function($routeProvider,$locationProvider){
         $routeProvider
             .when('/maintainNetwork',{
                 templateUrl: 'app/templates/maintainNetwork.html' 
             });
+        
+        //$locationProvider.html5Mode(true);
     });
