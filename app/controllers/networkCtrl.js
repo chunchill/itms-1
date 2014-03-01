8f41a3e408e22dc55c4cf56412c19de3a4a25840
sean <xujihui1985@outlook.com>
2014-03-01 18:58:43 +0800

    create networkCtrl for network section and mock the ui with scope.orderlist

---
+++ app/controllers/networkCtrl.js
@@ -0,0 +1,42 @@
+angular.module('itmsApp').controller('networkCtrl', function($scope) {
+    $scope.orderlist = [{
+        number: 'EO001', 
+        er: 1,
+        eritn: '001',
+        type: '预付',
+        special: 'nomal',
+        delivery: 'General',
+        receiver: 'nanjin',
+        provice: 'jiangsu',
+        city: 'nanjing',
+        distact: 'xiaguan',
+        deliveryDate: '2014-4-1',
+        deliveryMethod: 'fly'
+    },{
+        number: 'EO001', 
+        er: 1,
+        eritn: '001',
+        type: '预付',
+        special: 'nomal',
+        delivery: 'General',
+        receiver: 'nanjin',
+        provice: 'jiangsu',
+        city: 'nanjing',
+        distact: 'xiaguan',
+        deliveryDate: '2014-4-1',
+        deliveryMethod: 'fly'
+    },{
+        number: 'EO001', 
+        er: 1,
+        eritn: '001',
+        type: '预付',
+        special: 'nomal',
+        delivery: 'General',
+        receiver: 'nanjin',
+        provice: 'jiangsu',
+        city: 'nanjing',
+        distact: 'xiaguan',
+        deliveryDate: '2014-4-1',
+        deliveryMethod: 'fly'
+    }];
+});
