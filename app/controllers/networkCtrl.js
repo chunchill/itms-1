370fb480504d00f605f2d01c5038bf5eaac9ac9a
sean <xujihui1985@outlook.com>
2014-03-01 19:19:06 +0800

    replace href with data-target and format html

--- app/controllers/networkCtrl.js
+++ app/controllers/networkCtrl.js
@@ -1,3 +1,4 @@
+//by default, the selected order is the first order in the order list
 angular.module('itmsApp').controller('networkCtrl', function($scope) {
     $scope.orderlist = [{
         number: 'EO001', 
@@ -39,4 +40,11 @@ angular.module('itmsApp').controller('networkCtrl', function($scope) {
         deliveryDate: '2014-4-1',
         deliveryMethod: 'fly'
     }];
+
+    //by default, the selected order is the first order in the order list
+    $scope.selectedOrder = $scope.orderlist[0];
+
+    $scope.showDetail = function(order){
+        $scope.selectedOrder = order;
+    };
 });
